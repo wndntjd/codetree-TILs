62ab43e227e1f08f648f30aed98e35067aa751cd
wndntjd <124240540+wndntjd@users.noreply.github.com>
2024-06-25 10:44:47 +0900

    Title: 특정 숫자 도달하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240625/특정 숫자 도달하기/reaching-specific-number.cpp
+++ 240625/특정 숫자 도달하기/reaching-specific-number.cpp
@@ -2,10 +2,9 @@
 #include <cmath>
 using namespace std;
 int main() {
-    int n = 10;
     int sum = 0;
-    int i = 0;
-    for (i ; i<10; i++) {
+    int i;
+    for (i=0; i<10; ++i) {
         int k; cin >> k;
         if( k >= 250 ) break;
         sum += k;
